1393e9308e4e8ec0ade1ea70dbd4eb0df4ca392f
Pranav Ballaney <ballaneypranav@gmail.com>
2020-11-01 00:58:18 +0530

    Write postprocess

---
+++ utils/postprocess.py
@@ -0,0 +1,24 @@
+
+import os
+from bs4 import BeautifulSoup, Doctype
+
+with open('dist/Poster/index.html', 'r') as file:
+    soup = BeautifulSoup(file, 'html5lib')
+
+# remove <!DOCTYPE html>
+for item in soup.contents:
+    if isinstance(item, Doctype):
+        item.extract()
+
+with open('dist/index.bundle.js', 'r') as file:
+    script_file = file.read()
+
+# embed JS in HTML
+script_tag = soup.find('script')
+script_tag.string = script_file
+del script_tag.attrs['src']
+
+with open('dist/Poster/index.html', 'wb') as file:
+    file.write(soup.prettify('utf-8'))
+
+os.remove('dist/index.bundle.js')
