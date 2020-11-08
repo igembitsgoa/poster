74827235ed24197515f12c276d16a621d505bb25
Pranav Ballaney <ballaneypranav@gmail.com>
2020-11-08 21:53:48 +0530

    Add HP map, start making hover thing, iGEM-deploy

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,15 @@ window.$ = $;
 import "../css/featherlight.min.scss";
 import "./featherlight.min.js";
 
+$(document).ready(function () {
+  $(function () {
+    var $uttarakhand = $("#human_practices_map g#layer3");
+    $uttarakhand.on({
+      mouseenter: function () {
+        var $parent = $uttarakhand.parent();
+        // $uttarakhand.remove();
+        $parent.append($uttarakhand);
+      },
+    });
+  });
+});
