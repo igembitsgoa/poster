4ea99f2d1073967d12282adbfc243a3e1cf6db23
Pranav Ballaney <ballaneypranav@gmail.com>
2020-11-09 03:19:19 +0530

    HP clicky things work, added genetic circuits

--- src/js/main.js
+++ src/js/main.js
@@ -6,15 +6,27 @@ window.$ = $;
 import "../css/featherlight.min.scss";
 import "./featherlight.min.js";
 
+var state = null;
+
 $(document).ready(function () {
-  $(function () {
-    var $uttarakhand = $("#human_practices_map g#layer3");
-    $uttarakhand.on({
-      mouseenter: function () {
-        var $parent = $uttarakhand.parent();
-        // $uttarakhand.remove();
-        $parent.append($uttarakhand);
-      },
+  var states = [
+    "Uttarakhand",
+    "UttarPradesh",
+    "Maharashtra",
+    "TamilNadu",
+    "Karnataka",
+  ];
+  for (var i = 0; i < states.length; i++) {
+    $(document).on("click", "#" + states[i] + "_Marker", function () {
+      state = $(this).attr("id").split("_")[0];
+      toggle_states();
+
+      $("g").removeClass("clicked");
+      $(this).addClass("clicked");
     });
-  });
+  }
 });
+
+function toggle_states() {
+  $('body').attr('data-state', state);
+}
